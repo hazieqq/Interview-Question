f29d865b196a1d2e11625db25c6f87e4a53e40ac
Muhammad Hazieq <80628750+hazieqq@users.noreply.github.com>
2021-04-28 15:27:05 +0800

    Add files via upload

---
+++ InterviewQuestionUsingPython/interviewQ1.py
@@ -0,0 +1,30 @@
+
+try:
+    file = open("text.txt","r")
+    arr=[]
+    age=[]
+    line = file.read()
+    lines = line.strip()
+    a = lines.splitlines()
+    for i in range(len(a)):
+        if a[i] != "":
+            arr.append(a[i].replace(" ",""))
+    for i in range(len(arr)):
+        s = str(arr[i]).split('"')
+        for j in range(len(s)):
+            if j==0 or j==2:
+                c = str(s[j]).split(',')
+                if j==0:
+                    for k in range(len(c)):
+                        if k>=2:
+                            if c[k].isdigit():
+                                age.append(int(c[k]))
+                else:
+                    for k in range(len(c)):
+                        if c[k].isdigit():
+                            age.append(int(c[k]))
+    print("Max age=",max(age))
+                            
+    file.close()
+except FileNotFoundError:
+    print("file not dound")
